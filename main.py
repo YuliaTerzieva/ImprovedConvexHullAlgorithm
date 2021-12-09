d9d8e68883b0b2a14c8dca7ee9e4caf4746dc081
YuliaTerzieva <yulia.terzieva@gmail.com>
2021-12-09 13:55:34 +0100

    Updated boundary points and comments + arc representation idea

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-# Coordinates of all points, this is for the crossing grid
-coords = [(500, 100), (500, 6200), (10000, 6200), (10000, 100), (4350, 6201), (5220, 6203), (6120, 6202), (8000, 6201),
-          (5200, 4000), (5200, 4500), (5200, 5000), (5200, 5500), (5200, 6000), (6210, 4245)]
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from random import randint
+from numpy.random import choice
 
-# Setting minimum and maximum values for the boudaries
-x_min = y_min = 9999999
-x_max = y_max = 0
+# --------------- Initialization of Points Sets ---------------#
+CrossingCoord = [(500, 100), (500, 6200), (10000, 6200), (10000, 100), (4350, 6201), (5220, 6203), (6120, 6202),
+                 (8000, 6201), (5200, 4000), (5200, 4500), (5200, 5000), (5200, 5500), (5200, 6000), (6210, 4245)]
 
-# Plot the points
-for c in coords:
-    plt.scatter(c[0], c[1])
+PlaiserTakCoord = [(750, 500), (4500, 500), (8250, 500), (4500, 1125), (4500, 1750), (4500, 2375), (4500, 3000),
+                   (4500, 3625), (750, 5500), (8250, 5500)]
 
-    if c[0] < x_min:
-        x_min = c[0]
-    if c[0] > x_max:
-        x_max = c[0]
-    if c[1] < y_min:
-        y_min = c[1]
-    if c[1] > y_max:
-        y_max = c[1]
+WorkCoordinates = CrossingCoord
 
-# Invert the y axis to represent the grid on the site equally
-plt.gca().invert_yaxis()
+# --------------- Step 1 : Boundary Points Initialization ---------------#
+x_min = (min(WorkCoordinates, key=lambda t: t[0]))[0]
+x_max = (max(WorkCoordinates, key=lambda t: t[0]))[0]
+y_min = (min(WorkCoordinates, key=lambda t: t[1]))[1]
+y_max = (max(WorkCoordinates, key=lambda t: t[1]))[1]
+print('The boundaries are bottom left({}, {}), top left({}, {}), top right({}, {}), bottom right({}, {})'.format(
+    x_min, y_min, x_min, y_max, x_max, y_max, x_max, y_min))
 
-print(x_min, x_max, y_min, y_max)
 
-print(len(coords))
+# --------------- Step 2 : Selection of Starting Point and Direction ---------------#
 
-# Select a random starting point
-rand = randint(0, len(coordinates) - 1)
-print(rand)
-start = coords[rand]
-print(start)
+rand = randint(0, len(WorkCoordinates) - 1)
+startPoint = WorkCoordinates[rand]
+
+# Suggestion - We can represent the connections/arcs between points with a matrix N by N with N being the number of
+# points. If there is a connection with those points we put a 1, otherwise 0. We need to keep track of which points
+# are added to the path and which are not. Another idea is to create a class where every point would have a left
+# neightbour and a right one. That would be more time consuming tho.

0be864dab79d14ae97ee0ab71bdf5d6beb38e2a6
YuliaTerzieva <yulia.terzieva@gmail.com>
2021-12-13 13:25:37 +0100

    Working on gastalt, slope matrix added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from random import randint
+from scipy.spatial import distance
 from numpy.random import choice
 
 # --------------- Initialization of Points Sets ---------------#
@@ -11,23 +12,41 @@ CrossingCoord = [(500, 100), (500, 6200), (10000, 6200), (10000, 100), (4350, 62
 PlaiserTakCoord = [(750, 500), (4500, 500), (8250, 500), (4500, 1125), (4500, 1750), (4500, 2375), (4500, 3000),
                    (4500, 3625), (750, 5500), (8250, 5500)]
 
-WorkCoordinates = CrossingCoord
+SShaped = [(893, 2114), (4513, 3711), (4878, 3453), (5213, 3179), (5578, 2905), (928, 2631), (6232, 2418),
+           (6628, 2099), (6993, 1840), (7342, 1764), (7707, 1840), (7966, 2069)]
 
-# --------------- Step 1 : Boundary Points Initialization ---------------#
-x_min = (min(WorkCoordinates, key=lambda t: t[0]))[0]
-x_max = (max(WorkCoordinates, key=lambda t: t[0]))[0]
-y_min = (min(WorkCoordinates, key=lambda t: t[1]))[1]
-y_max = (max(WorkCoordinates, key=lambda t: t[1]))[1]
-print('The boundaries are bottom left({}, {}), top left({}, {}), top right({}, {}), bottom right({}, {})'.format(
-    x_min, y_min, x_min, y_max, x_max, y_max, x_max, y_min))
+Dantzig = [(2100, 600), (6900, 5400), (5700, 4200), (4500, 3000), (5700, 3000), (6300, 3000), (6900, 3000),
+           (3300, 1800), (2100, 5400), (6900, 600)]
+
+WorkCoordinates = SShaped
+# --------------- Slopes between lines ---------------#
+slopes = np.array([["{:.2f}".format((a[1]-b[1])/(a[0]-b[0])) if a[0]!=b[0] else None for a in WorkCoordinates]
+                   for b in WorkCoordinates])
+
+print(slopes)
 
 
-# --------------- Step 2 : Selection of Starting Point and Direction ---------------#
-
-rand = randint(0, len(WorkCoordinates) - 1)
-startPoint = WorkCoordinates[rand]
-
-# Suggestion - We can represent the connections/arcs between points with a matrix N by N with N being the number of
-# points. If there is a connection with those points we put a 1, otherwise 0. We need to keep track of which points
-# are added to the path and which are not. Another idea is to create a class where every point would have a left
-# neightbour and a right one. That would be more time consuming tho.
+# # --------------- Step 1 : Boundary Points Initialization ---------------#
+# x_min = (min(WorkCoordinates, key=lambda t: t[0]))[0]
+# x_max = (max(WorkCoordinates, key=lambda t: t[0]))[0]
+# y_min = (min(WorkCoordinates, key=lambda t: t[1]))[1]
+# y_max = (max(WorkCoordinates, key=lambda t: t[1]))[1]
+# # print('The boundaries are bottom left({}, {}), top left({}, {}), top right({}, {}), bottom right({}, {})'.format(
+# #     x_min, y_min, x_min, y_max, x_max, y_max, x_max, y_min))
+#
+# distances = np.array([[distance.euclidean(a, b) for a in WorkCoordinates] for b in WorkCoordinates])
+# # print(distances)
+#
+# average = distances.mean()
+# # print(average)
+#
+# cluster = []
+# for i, row in enumerate(distances):
+#     if (row.mean() < average):
+#         cluster.append(WorkCoordinates[i])
+#
+# print(cluster)
+# # --------------- Step 2 : Selection of Starting Point and Direction ---------------#
+#
+# rand = randint(0, len(WorkCoordinates) - 1)
+# startPoint = WorkCoordinates[rand]
